80c4cc8d97163fc5ac3fbe8d7508692424c3dbe6
Justus-Onyancha <justus.onyancha@moringaschool.com>
2022-03-20 16:54:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(text="roller-skate"){
+    return `This Saturday, I want to ${text}!`
+}
+
+function mondayWork(text="go to the office"){
+   return `This Monday, I will ${text}.`
+}
+
+let wrapAdjective = function(text="*"){
+   return function(adjective="special"){
+       return `You are ${text}${adjective}${text}!`
+   } 
+}
+
